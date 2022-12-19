c7b2663904bfb33fad3bcf8bdf4f8ed993ece913
Wojciech Kwolek <wojciech@kwolek.io>
2022-12-19 08:53:20 +0100

    add formula for spa

---
+++ Formula/spa.rb
@@ -0,0 +1,21 @@
+require 'formula'
+class Spa < Formula
+  homepage 'https://github.com/irth/spa'
+  version '0.1'
+
+  if Hardware::CPU.arm?
+    url 'https://github.com/irth/spa/releases/download/v1.0/spa.darwin.arm64'
+    sha256 "9e0bbfeffdac66547933d88c0a5b509eaf4f03823d7b7d69434235b7ca58fc2b"
+  else
+    url 'https://github.com/irth/spa/releases/download/v1.0/spa.darwin.amd64'
+    sha256 '9be085e4037592396e18dd8d5cbb893566f6b71ccf99ecae3c01417e4bd95ec2'
+  end
+
+  def install
+    if Hardware::CPU.arm?
+      bin.install "spa.darwin.arm64" => "spa"
+    else
+      bin.install "spa.darwin.amd64" => "spa"
+    end
+  end
+end
