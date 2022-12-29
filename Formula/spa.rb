0e04b942f34ebe08bf5d4ca2f006bbcc7a6abcce
Wojciech Kwolek <wojciech@kwolek.io>
2022-12-29 01:38:17 +0100

    spa v1.1

--- Formula/spa.rb
+++ Formula/spa.rb
@@ -1,14 +1,14 @@
 require 'formula'
 class Spa < Formula
   homepage 'https://github.com/irth/spa'
-  version '1.0'
+  version '1.1'
 
   if Hardware::CPU.arm?
-    url 'https://github.com/irth/spa/releases/download/v1.0/spa.darwin.arm64'
-    sha256 "9e0bbfeffdac66547933d88c0a5b509eaf4f03823d7b7d69434235b7ca58fc2b"
+    url 'https://github.com/irth/spa/releases/download/v1.1/spa.darwin.arm64'
+    sha256 '56d742a6c4810be433da602ea662eb6beed1a55ee4e10e09eb71a29abc864729'
   else
-    url 'https://github.com/irth/spa/releases/download/v1.0/spa.darwin.amd64'
-    sha256 '9be085e4037592396e18dd8d5cbb893566f6b71ccf99ecae3c01417e4bd95ec2'
+    url 'https://github.com/irth/spa/releases/download/v1.1/spa.darwin.amd64'
+    sha256 '4ff0ee18993ab77196c5d4f30793929be4981465d6231ce208bd8d92f71d5a81'
   end
 
   def install
